dfdcd8b5b826c1d6a9de5b999a4548d969d3b130
Wrya Baxtyar <wrya141@gmail.com>
2025-05-10 09:29:28 +0300

    color toggle changer

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ stuff i built from huxn webdev tutorials
 01.Background-image  
 02. creative image hover effect  
 03. custom scroll bar  
-04. loading animatin
+04. loading animatin  
+05. color toggle changer
---
+++ color toggle changer/app.js
@@ -0,0 +1,8 @@
+let toggler = document.getElementById("switch");
+toggler.addEventListener("click", () => {
+  if (toggler.checked) {
+    document.body.style.backgroundColor = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+});
